58bb3d157b1047823bf884024b1ac63b10a0dfc3
LDanic <vadani1234@gmail.com>
2025-04-06 21:11:49 -0500

    organizacion en carpeticas :)

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/dani/projects/Espacio de estados caballos/venv'
+set -gx VIRTUAL_ENV '/home/dani/projects/Talleres IA II/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
